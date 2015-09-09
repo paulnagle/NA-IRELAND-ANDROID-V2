848af79c975808135372775a2e154440f4af5b2e
paulnagle <paulnagle@gmail.com>
2015-09-09 22:19:17 +0100

    Fixed the different redirections tel: mailto: http:

--- java/ie/nasouth/android/naireland/MainActivity.java
+++ java/ie/nasouth/android/naireland/MainActivity.java
@@ -3,7 +3,6 @@ package ie.nasouth.android.naireland;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.ActionBarActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
@@ -36,6 +35,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+
+    // This is the starting html page for the app
     private final String url = "file:///android_asset/index.html";
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -47,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         webView = (WebView) findViewById(R.id.activity_main_webview);
 
-        // Needed settings, that are false be default:
+        // Needed settings, that are false by default:
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setBuiltInZoomControls(true);
@@ -61,42 +62,67 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Forces web view not to re-direct to a browser when
-        // clicking on a link, except for google maps:
-        // TODO Dont override links to area websites linked from the app?
+        // clicking on a link, except for google maps, or any of the
+        // area/region websites.
+        // Also this forces tel: links to open the android dialer,
+        // and forces mailto: links to open an email
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
 
-                // Override everything but external links
-                if ((Uri.parse(url).getHost().equals("maps.google.com")) ||
-                    (Uri.parse(url).getHost().equals("www.na-ireland.org")) ||
-                    (Uri.parse(url).getHost().equals("www.naeasternarea.org")) ||
-                    (Uri.parse(url).getHost().equals("www.nanorthernireland.com")) ||
-                    (Uri.parse(url).getHost().equals("github.com")) ) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    startActivity(intent);
+                // Redirect all tel: links to the dialler
+                if (url.startsWith("tel:")) {
+                    Intent tel = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                    startActivity(tel);
                     return true;
                 }
-                if (url.endsWith(".mp3")) {
 
+                // Redirect all mailto: links to the email app
+                if (url.startsWith("mailto:")) {
+                    Intent i = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                    startActivity(i);
+                    return true;
+                }
+
+                // The mp3 files are currently located on www.nasouth.ie, so
+                // handle mp3 extension files before redirecting the area/region
+                // websites.
+                if (url.endsWith(".mp3")) {
                     Intent intent = new Intent(MainActivity.this, AudioPlayer.class);
                     Bundle b = new Bundle();
                     b.putString("AUDIO_FILE_NAME", url);
                     intent.putExtras(b);
                     startActivity(intent);
                     return true;
+
                 }
+
+                // Redirect all of the external links to the system web browser
+                if (    (Uri.parse(url).getHost().equals("maps.google.com")) ||
+                        (Uri.parse(url).getHost().equals("www.na-ireland.org")) ||
+                        (Uri.parse(url).getHost().equals("www.naeasternarea.org")) ||
+                        (Uri.parse(url).getHost().equals("www.nanorthernireland.com")) ||
+                        (Uri.parse(url).getHost().equals("www.nasouth.ie")) ||
+                        (Uri.parse(url).getHost().equals("github.com"))) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+
                 return false;
             }
-
         });
 
+
+        // As this app relies heavily on having a working connection to the internet, we will
+        // display a message if no internet connection is available
         ConnectivityManager cm =  (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = (activeNetwork != null) && (activeNetwork.isConnectedOrConnecting());
 
         if (!isConnected) {
-            Toast.makeText(getApplicationContext(), "No Internet access detected!!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "No Internet access detected!! This app relies on having a working internet connection.", Toast.LENGTH_LONG).show();
         }
 
         // Load the web-page:
@@ -179,7 +205,6 @@ public class MainActivity extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy(); // Always call the superclass method first
 
-        webView.loadUrl("about:blank");
         webView.stopLoading();
         webView.setWebChromeClient(null);
         webView.setWebViewClient(null);
