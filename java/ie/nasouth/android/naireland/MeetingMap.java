557c8b88edced3eade182abd3016d2f5705d7470
paulnagle <paulnagle@gmail.com>
2015-09-11 12:09:11 +0100

    Google Map now displays a marker for each meeting

--- java/ie/nasouth/android/naireland/MeetingMap.java
+++ java/ie/nasouth/android/naireland/MeetingMap.java
@@ -1,15 +1,37 @@
 package ie.nasouth.android.naireland;
 
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class MeetingMap extends FragmentActivity {
+    private static final String TAG = "MeetingMap";
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
@@ -63,10 +85,122 @@ public class MeetingMap extends FragmentActivity {
      */
     private void setUpMap() {
         LatLng center = new LatLng(53.341318, -6.270205);
+
         mMap.setMyLocationEnabled(true);
         mMap.setBuildingsEnabled(true);
-        mMap.addMarker(new MarkerOptions().position(center).title("NA Ireland Service Office"));
+
+        UiSettings mapSettings;
+        mapSettings = mMap.getUiSettings();
+        mapSettings.setZoomControlsEnabled(true);
+
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 17));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(7), 2000, null);
+
+        new retrieveAndAddMeetings().execute();
+    }
+
+    public String getJSON(String url, int timeout) {
+        HttpURLConnection c = null;
+        try {
+            URL u = new URL(url);
+            c = (HttpURLConnection) u.openConnection();
+            c.setRequestMethod("GET");
+            c.setRequestProperty("Content-length", "0");
+            c.setUseCaches(false);
+            c.setAllowUserInteraction(false);
+            c.setConnectTimeout(timeout);
+            c.setReadTimeout(timeout);
+            c.connect();
+            int status = c.getResponseCode();
+
+            switch (status) {
+                case 200:
+                case 201:
+                    BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line).append("\n");
+                    }
+                    br.close();
+                    return sb.toString();
+            }
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (c != null) {
+                try {
+                    c.disconnect();
+                } catch (Exception ex) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return null;
+    }
+
+
+    class retrieveAndAddMeetings extends AsyncTask< Void, Void, JSONArray > {
+        JSONObject meeting;
+
+        protected JSONArray doInBackground(Void... args) {
+            List params = new ArrayList();
+            // getting JSON string from URL
+
+            final String BMLTJson;
+            JSONObject meeting;
+
+            BMLTJson = getJSON("http://bmlt.nasouth.ie/main_server/client_interface/json/?switcher=GetSearchResults", 30000);
+
+            try {
+
+                JSONArray BMLTResults =  new JSONArray(BMLTJson);
+                return BMLTResults;
+
+            } catch (JSONException e) {
+                Log.d(TAG, "Gone wrong there!" + e);
+            }
+        return null;
+        }
+
+        protected void onPostExecute(JSONArray BMLTResults) {
+            Double meetingLongitude;
+            Double meetingLatitude;
+            String meetingName;
+            String meetingLocation;
+            String meetingStreet;
+            String meetingCounty;
+            String meetingDay;
+            String meetingStart;
+            LatLng center = new LatLng(53.341318, -6.270205);
+
+            mMap.addMarker(new MarkerOptions().position(center).title("NA Ireland Service Office"));
+
+            for (int i=0; i < BMLTResults.length(); i++) {
+                try {
+                    meeting = BMLTResults.getJSONObject(i);
+
+                    meetingLongitude = meeting.getDouble("longitude");
+                    meetingLatitude  = meeting.getDouble("latitude");
+                    meetingName      = meeting.getString("meeting_name");
+                    meetingLocation  = meeting.getString("location_text");
+                    meetingStreet    = meeting.getString("location_street");
+                    meetingCounty    = meeting.getString("location_sub_province");
+                    meetingDay       = meeting.getString("weekday_tinyint");
+                    meetingStart     = meeting.getString("start_time");
+
+ //                   Log.d(TAG, meetingLongitude + " " + meetingLatitude + " " + meetingName + " " + meetingLocation + " " + meetingStreet + " " + meetingCounty);
+
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(meetingLatitude, meetingLongitude)));
+
+                } catch (JSONException e) {
+                    Log.d(TAG, "Gone wrong here!" + e);
+                }
+            }
+        }
     }
 }
